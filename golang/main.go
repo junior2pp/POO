a2c5f4b7ceb1439ef24a1ddf13e59043b12075b7
junior2pp <junior2pp@hotmail.com>
2017-06-05 23:03:06 -0500

    Package hecho en goalng para la maquina de tickets

--- golang/main.go
+++ golang/main.go
@@ -1,6 +1,30 @@
-
 package main
 
-func main()  {
+import (
+	"fmt"
 
+	"github.com/junior2pp/MaquinaTicket"
+)
+
+func main() {
+
+	m := new(maquina.Ticket)
+	m.Init(10)
+	precio := m.GetPrecio()
+	fmt.Println("PRECIO DEL TICKET")
+	fmt.Println(precio)
+	m.SetBalance(50)
+	fmt.Println("BALANCE")
+	fmt.Println(m.GetBalance())
+	//comprar ticket
+	if m.GetBalance() >= m.GetPrecio() {
+		m.IncrementarContador()
+		m.SetTotal(m.GetPrecio())
+		m.SetBalance((m.GetBalance() - m.GetPrecio()))
+		contador, precio := m.ImprimirTicket()
+		fmt.Println("TICKET #", contador)
+		fmt.Println("PRECIO", precio)
+	}
+	fmt.Println(m.GetTotal())
+	fmt.Println(m.GetBalance())
 }
